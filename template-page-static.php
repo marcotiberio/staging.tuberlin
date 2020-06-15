baa0ffc564e0f1a8f7394acf5f02830a20e08fb3
Marco <marco@defrostudio.eu>
2020-06-15 14:04:32 +0200

    static page template

---
+++ template-page-static.php
@@ -0,0 +1,35 @@
+<?php 
+	/* Template Name: Page Static */ 
+?>
+
+<?php get_header(); ?>
+
+	<div id="primary--page-main" class="content-area--page-main">
+		<main id="main--page-main" class="site-main--page-main">
+
+        <section class="page-static-layout">
+            <div class="page-left"></div>
+                <div class="column1">
+                    <h3>Kontakt:</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+                        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
+                <div class="column2">
+                    <h3>Standort:</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+                        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
+                        dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
+                        mollit anim id est laborum.</p>
+                </div>
+            <div class="page-right"></div>
+        </section>
+        
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
