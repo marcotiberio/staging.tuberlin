378167aef59e98e88cf8c107865ad028416f107a
Marco <marco@defrostudio.eu>
2020-06-16 11:13:52 +0200

    new template three columns

---
+++ template_staticpage_threecol.php
@@ -0,0 +1,45 @@
+<?php 
+	/* Template Name: Static Page - Two Columns */ 
+?>
+
+<?php get_header(); ?>
+
+	<div id="primary--page-main" class="content-area--page-main">
+		<main id="main--page-main" class="site-main--page-main">
+
+        <section class="page-static-layout_3">
+                <div class="column1">
+                    <?php the_field('column-one_staticpageIII'); ?>
+                    <!-- <h3>Kontakt:</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+                        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p> -->
+                </div>
+                <div class="column2">
+                    <?php the_field('column-two_staticpageIII'); ?>
+                    <!-- <h3>Standort:</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+                        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
+                        dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
+                        mollit anim id est laborum.</p> -->
+                </div>
+                <div class="column3">
+                    <?php the_field('column-three_staticpageIII'); ?>
+                    <!-- <h3>Standort:</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+                        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
+                        dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
+                        mollit anim id est laborum.</p> -->
+                </div>
+            </div>
+        </section>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
