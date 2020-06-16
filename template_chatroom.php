1280b5915b1afe59c62ff28ee6e0b390fd6d63d2
Marco <marco@defrostudio.eu>
2020-06-16 12:02:12 +0200

    test ig embed

--- template_chatroom.php
+++ template_chatroom.php
@@ -7,17 +7,20 @@
 	<div id="primary--page-main" class="content-area--page-main">
 		<main id="main--page-main" class="site-main--page-main">
 
-        <section class="page-chatroom">
-            <div class="instagram-post_container">
-                <?php the_field('instagram_post'); ?>
-            </div>
-            <div class="instagram-post_container">
-                <?php the_field('instagram_post'); ?>
-            </div>
-            <div class="instagram-post_container">
-                <?php the_field('instagram_post'); ?>
-            </div>
-        </section>
+            <section class="page-chatroom">
+                <div class="instagram-post_container">
+                    <?php $instagram_url = get_field( 'instagram_post', false, false ); ?>
+                    <img src="<?php echo $instagram_url; ?>media/?size=l" />
+                </div>
+                <div class="instagram-post_container">
+                    <?php $instagram_url = get_field( 'instagram_post', false, false ); ?>
+                    <img src="<?php echo $instagram_url; ?>media/?size=l" />
+                </div>
+                <div class="instagram-post_container">
+                    <?php $instagram_url = get_field( 'instagram_post', false, false ); ?>
+                    <img src="<?php echo $instagram_url; ?>media/?size=l" />
+                </div>
+            </section>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
