93e8fe7c4f1925c0e21bf777e561aff6a9f682d1
Marco <marco@defrostudio.eu>
2020-06-16 14:30:06 +0200

    fixed landing page content

--- template_landingpage.php
+++ template_landingpage.php
@@ -6,6 +6,20 @@
 
 <section class="background_landing">
 
+    <?php
+		while ( have_posts() ) :
+			the_post();
+
+			get_template_part( 'template-parts/content', 'page' );
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+
+		endwhile; // End of the loop.
+	?>
+
     <div class="button_logo-landing">
 		<a href="http://staging-tuberlinifa.kinsta.cloud/ifa">
             <img src="http://staging-tuberlinifa.kinsta.cloud/wp-content/uploads/2020/06/logo.png" alt="TU Berlin IfA Logo">
