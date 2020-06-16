9331d509e9c60ce241fc1e05ed54f8c63c413d39
Marco <marco@defrostudio.eu>
2020-06-16 17:03:18 +0200

    font in the header?

--- header.php
+++ header.php
@@ -16,6 +16,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap" rel="stylesheet">
 
 	<?php wp_head(); ?>
 </head>
