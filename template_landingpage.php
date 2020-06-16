b453537b59db1d10e701cbb4b4d7998d0f09117c
Marco <marco@defrostudio.eu>
2020-06-16 12:52:16 +0200

    landing page design

--- template_landingpage.php
+++ template_landingpage.php
@@ -5,13 +5,9 @@
 <?php get_header(); ?>
 
 <section class="background_landing">
-
-    <div class="button_logo-landing">
-		<a href="http://staging-tuberlinifa.kinsta.cloud/">
-            <img src="http://staging-tuberlinifa.kinsta.cloud/wp-content/uploads/2020/06/logo.png" alt="TU Berlin IfA Logo">
-        </a>
-    </div>
-
+	<a href="http://staging-tuberlinifa.kinsta.cloud/">
+        <img src="http://staging-tuberlinifa.kinsta.cloud/wp-content/uploads/2020/06/logo.png" alt="TU Berlin IfA Logo">
+    </a>
 </section>
 
 
