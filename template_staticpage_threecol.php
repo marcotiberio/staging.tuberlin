96438bd7154874ddb04509ae575191eae8916041
Marco <marco@defrostudio.eu>
2020-06-16 11:20:51 +0200

    fixed layout 3 columns

--- template_staticpage_threecol.php
+++ template_staticpage_threecol.php
@@ -8,33 +8,32 @@
 		<main id="main--page-main" class="site-main--page-main">
 
         <section class="page-static-layout_3">
-                <div class="column1">
-                    <?php the_field('column-one_staticpageIII'); ?>
-                    <!-- <h3>Kontakt:</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-                        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
-                        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p> -->
-                </div>
-                <div class="column2">
-                    <?php the_field('column-two_staticpageIII'); ?>
-                    <!-- <h3>Standort:</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-                        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
-                        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
-                        dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
-                        mollit anim id est laborum.</p> -->
-                </div>
-                <div class="column3">
-                    <?php the_field('column-three_staticpageIII'); ?>
-                    <!-- <h3>Standort:</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-                        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
-                        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
-                        dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
-                        mollit anim id est laborum.</p> -->
-                </div>
+            <div class="column1">
+                <?php the_field('column-one_staticpageIII'); ?>
+                <!-- <h3>Kontakt:</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+                    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p> -->
+            </div>
+            <div class="column2">
+                <?php the_field('column-two_staticpageIII'); ?>
+                <!-- <h3>Standort:</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+                    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
+                    dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
+                    mollit anim id est laborum.</p> -->
+            </div>
+            <div class="column3">
+                <?php the_field('column-three_staticpageIII'); ?>
+                <!-- <h3>Standort:</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+                    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
+                    dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
+                    mollit anim id est laborum.</p> -->
             </div>
         </section>
 		</main><!-- #main -->
