cc416e51467d408e9a7b40e419940bd07f8cd03b
Marco <marco@defrostudio.eu>
2020-06-16 13:41:37 +0200

    added fade-in class

--- js/transition.js
+++ js/transition.js
@@ -2,8 +2,8 @@ jQuery( document ).ready(function($) {
     $('body').addClass("fade-in");
 });
 
-jQuery( document ).ready(function($) {
-    $("#post-credits_mobile-title").click( function() { 
-        $("#toggle-credits-mobile").toggle();
-    } );
-});
+// jQuery( document ).ready(function($) {
+//     $("#post-credits_mobile-title").click( function() { 
+//         $("#toggle-credits-mobile").toggle();
+//     } );
+// });
