601b35cfa9ce32e0fb785fce8cfdc12701d73477
Marco <marco@defrostudio.eu>
2020-06-17 17:54:15 +0200

    renamed

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@
 // });
 
 // Get the modal
-var modal = document.getElementById("modalGallery");
+var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
 var btn = document.getElementById("openGallery");
