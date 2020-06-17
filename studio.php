62f14d6224744bd35dbb0d067b8c36f06199ffa1
Marco <marco@defrostudio.eu>
2020-06-17 14:21:08 +0200

    template post studio

---
+++ studio.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ * Template Name: Studio
+ * Template Post Type: post, page, product
+ */
+  
+ get_header();  ?>
+
+<div class="wrap">
+    
+    <div id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+		<?php
+		while ( have_posts() ) :
+			the_post();
+
+			get_template_part( 'template-parts/content-custom', get_post_type() );
+
+		endwhile; // End of the loop.
+		?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+    
+    <!-- CUSTOM POST LAYOUT -->
+    <!-- CUSTOM POST LAYOUT -->
+    <!-- CUSTOM POST LAYOUT -->
+
+
+<?php
+get_footer(); ?>
