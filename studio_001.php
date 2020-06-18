7e68fb3da736e132a17665825d796f811a36f172
Marco <marco@defrostudio.eu>
2020-06-18 13:36:56 +0200

    test

--- studio_001.php
+++ studio_001.php
@@ -89,7 +89,7 @@
         <div class="modal-content">
             <span class="close">&times;</span>            
                 <?php the_field('info_studio'); ?>
-                <h1>PREVIEW</h1>
+            <h1 style="display: block;">PREVIEW</h1>
         </div>
 
     </div>
