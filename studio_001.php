af9d1f92018f5dcb65eb749eb9e3ef925b40dced
Marco <marco@defrostudio.eu>
2020-06-18 14:14:30 +0200

    test

--- studio_001.php
+++ studio_001.php
@@ -99,31 +99,31 @@
 
         <div class="modal-content">
             <span class="close">&times;</span>            
-            <div class="embed-container">
-    <?php the_field('video_studio'); ?>
-</div>
-<style>
-    .embed-container { 
-        position: relative; 
-        padding-bottom: 56.25%;
-        overflow: hidden;
-        max-width: 100%;
-        height: auto;
-    } 
+            <?php
 
-    .embed-container iframe,
-    .embed-container object,
-    .embed-container embed { 
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-    }
-</style>
-        </div>
+            // Load value.
+            $iframe = get_field('video_studio');
 
-    </div>
+            // Use preg_match to find iframe src.
+            preg_match('/src="(.+?)"/', $iframe, $matches);
+            $src = $matches[1];
+
+            // Add extra parameters to src and replcae HTML.
+            $params = array(
+                'controls'  => 0,
+                'hd'        => 1,
+                'autohide'  => 1
+            );
+            $new_src = add_query_arg($params, $src);
+            $iframe = str_replace($src, $new_src, $iframe);
+
+            // Add extra attributes to iframe HTML.
+            $attributes = 'frameborder="0"';
+            $iframe = str_replace('></iframe>', ' ' . $attributes . '></iframe>', $iframe);
+
+            // Display customized HTML.
+            echo $iframe;
+            ?>
 
 
 <?php
