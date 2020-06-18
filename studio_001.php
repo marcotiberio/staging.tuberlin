5f8f0f47a43734314f55431df076ac8af636b5cf
Marco <marco@defrostudio.eu>
2020-06-18 17:47:42 +0200

    play button

--- studio_001.php
+++ studio_001.php
@@ -89,7 +89,10 @@
         <div class="modal-content">
             <span class="close">&times;</span>            
                 <?php the_field('info_studio'); ?>
-            <div id="openVideo" style="display: block;"><h3>Interview</h3></div>
+            <div id="openVideo" style="display: block;">
+                <span><img src="http://staging-tuberlinifa.kinsta.cloud/wp-content/uploads/2020/06/play-button.png" alt="Play Button"></span>
+                <h3>Interview</h3>
+            </div>
         </div>
 
     </div>
