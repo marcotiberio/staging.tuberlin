ce7dff97065e5703d942a3ec1c68b7f994517357
Marco <marco@defrostudio.eu>
2020-06-18 17:50:47 +0200

    open Video

--- studio_001.php
+++ studio_001.php
@@ -89,7 +89,7 @@
         <div class="modal-content">
             <span class="close">&times;</span>            
                 <?php the_field('info_studio'); ?>
-            <div id="openVideo" style="display: block;">
+            <div id="openVideo">
                 <span><img src="http://staging-tuberlinifa.kinsta.cloud/wp-content/uploads/2020/06/play-button.png" alt="Play Button"></span>
                 <h3>Interview</h3>
             </div>
