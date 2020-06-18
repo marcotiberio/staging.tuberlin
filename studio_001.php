1f160c3dfc68950aa41a1385903174b2c31a2fa6
Marco <marco@defrostudio.eu>
2020-06-18 14:29:25 +0200

    test

--- studio_001.php
+++ studio_001.php
@@ -99,9 +99,13 @@
 
         <div class="modal-content">
             <span class="close">&times;</span>            
-            <div class="embed-container">
-                <?php the_field('oembed'); ?>
-            </div>
+            <?php
+            $youtube = get_field('video_studio');
+
+            if ('' !== strval($youtube)) {
+                echo '<iframe width="420" height="315" src="https://www.youtube.com/embed/' . $youtube . '"></iframe>';
+            }
+        ?>
         </div>
 
 
