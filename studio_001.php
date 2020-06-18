200e66941a1f1bfb560fa9101f24b4ac871a9220
Marco <marco@defrostudio.eu>
2020-06-18 10:53:05 +0200

    test gallery studio acf

--- studio_001.php
+++ studio_001.php
@@ -63,14 +63,20 @@
 
         <div class="modal-content">
             <span class="close">&times;</span>
-            <p>Gallery</p>
-            <p>Gallery</p>
-            <p>Gallery</p>
-            <p>Gallery</p>
-            <p>Gallery</p>
-            <p>Gallery</p>
-            <p>Gallery</p>
-            <p>Gallery</p>
+            <?php 
+            $images = get_field('gallery_studio');
+            if( $images ): ?>
+                <ul>
+                    <?php foreach( $images as $image ): ?>
+                        <li>
+                            <a href="<?php echo $image['url']; ?>">
+                                <img src="<?php echo $image['sizes']['thumbnail']; ?>" alt="<?php echo $image['alt']; ?>" />
+                            </a>
+                            <p><?php echo $image['caption']; ?></p>
+                        </li>
+                    <?php endforeach; ?>
+                </ul>
+            <?php endif; ?>
         </div>
 
     </div>
