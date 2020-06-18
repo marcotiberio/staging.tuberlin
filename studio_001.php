03847313001ab825fd88f76654e128431ba87c61
Marco <marco@defrostudio.eu>
2020-06-18 10:55:13 +0200

    gallery layout

--- studio_001.php
+++ studio_001.php
@@ -63,20 +63,22 @@
 
         <div class="modal-content">
             <span class="close">&times;</span>
-            <?php 
-            $images = get_field('gallery_studio');
-            if( $images ): ?>
-                <ul>
-                    <?php foreach( $images as $image ): ?>
-                        <li>
-                            <a href="<?php echo $image['url']; ?>">
-                                <img src="<?php echo $image['sizes']['thumbnail']; ?>" alt="<?php echo $image['alt']; ?>" />
-                            </a>
-                            <p><?php echo $image['caption']; ?></p>
-                        </li>
-                    <?php endforeach; ?>
-                </ul>
-            <?php endif; ?>
+            <div class="gallery_studio">
+                <?php 
+                $images = get_field('gallery_studio');
+                if( $images ): ?>
+                    <ul>
+                        <?php foreach( $images as $image ): ?>
+                            <li>
+                                <a href="<?php echo $image['url']; ?>">
+                                    <img src="<?php echo $image['sizes']['thumbnail']; ?>" alt="<?php echo $image['alt']; ?>" />
+                                </a>
+                                <p><?php echo $image['caption']; ?></p>
+                            </li>
+                        <?php endforeach; ?>
+                    </ul>
+                <?php endif; ?>
+            </div>
         </div>
 
     </div>
