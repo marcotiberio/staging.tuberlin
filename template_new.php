cd1534e5a0b4904ab8c58ad656ceef355afc2c17
Marco <marco@defrostudio.eu>
2020-06-18 11:05:46 +0200

    added template news

---
+++ template_new.php
@@ -0,0 +1,18 @@
+<?php 
+	/* Template Name: News */ 
+?>
+
+<?php get_header(); ?>
+
+	<div id="primary--page-main" class="content-area--page-main">
+		<main id="main--page-main" class="site-main--page-main">
+
+            <section class="page-news">
+                
+            </section>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
