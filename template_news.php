89b57f5aee7001c74ecf502212b9e659751447e9
Marco <marco@defrostudio.eu>
2020-06-18 11:18:09 +0200

    gllery news acf

--- template_news.php
+++ template_news.php
@@ -8,7 +8,31 @@
 		<main id="main--page-main" class="site-main--page-main">
 
             <section class="page-news">
-                
+                <div class="gallery-news">
+				<?php 
+				$images = get_field('gallery_news');
+				if( $images ): ?>
+					<div id="slider" class="flexslider">
+						<ul class="slides">
+							<?php foreach( $images as $image ): ?>
+								<li>
+									<img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+									<p><?php echo esc_html($image['caption']); ?></p>
+								</li>
+							<?php endforeach; ?>
+						</ul>
+					</div>
+					<div id="carousel" class="flexslider">
+						<ul class="slides">
+							<?php foreach( $images as $image ): ?>
+								<li>
+									<img src="<?php echo esc_url($image['sizes']['thumbnail']); ?>" alt="Thumbnail of <?php echo esc_url($image['alt']); ?>" />
+								</li>
+							<?php endforeach; ?>
+						</ul>
+					</div>
+				<?php endif; ?>
+				</div>
             </section>
 		</main><!-- #main -->
 	</div><!-- #primary -->
