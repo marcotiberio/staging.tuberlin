4ad82e37675019362499f65a07456fa7b6df5d4e
Marco <marco@defrostudio.eu>
2020-06-19 18:21:03 +0200

    template foyer

---
+++ template_foyer.php
@@ -0,0 +1,18 @@
+<?php 
+	/* Template Name: Foyer */ 
+?>
+
+<?php get_header(); ?>
+
+	<div id="primary--page-main" class="content-area--page-main">
+		<main id="main--page-main" class="site-main--page-main">
+
+            <section class="page-foyer">
+                <img src="assets/img/foyer.svg" alt="" style="display: block;">
+            </section>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
