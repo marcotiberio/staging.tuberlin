0ccbc27d7c3a3bd72b80983ed3cbd52c540fd549
Marco <marco@defrostudio.eu>
2020-06-20 14:47:03 +0200

    ID FLIRST FLOOR

--- template_foyer.php
+++ template_foyer.php
@@ -14,7 +14,7 @@
           <div><a href="#">4</a></div>
           <div><a href="#">5</a></div>
           <div><a href="#">6</a></div>
-          <div><a href="#">1</a></div>
+          <div><a href="#firstFloor">1</a></div>
           <div><a href="#secondFloor">2</a></div>
           <div><a href="#">3</a></div>
           <div><a href="#"></a></div>
