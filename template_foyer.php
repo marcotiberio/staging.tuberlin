eca24544809582224e82e4b12e4177ddf02e7a81
Marco <marco@defrostudio.eu>
2020-06-20 14:10:21 +0200

    floors

--- template_foyer.php
+++ template_foyer.php
@@ -9,7 +9,7 @@
 
         <section class="page-foyer">
 
-          <div class="floor1">
+          <div class="floor">
             <svg xmlns="http://www.w3.org/2000/svg" id="svgFloor1" viewBox="0 0 1197.871 752.334">
               <g id="Group_1510" data-name="Group 1510" transform="translate(-388.728 -139.211)">
                 <g id="_00_ACKERSTR" data-name="00_ACKERSTR" transform="translate(389.728 527.779)">
@@ -469,8 +469,8 @@
               </g>
             </svg>
           </div>
-          <div class="floor2">
-            <svg xmlns="http://www.w3.org/2000/svg" id="svgFloor1" viewBox="0 0 1197.871 752.334">
+          <div class="floor">
+            <svg xmlns="http://www.w3.org/2000/svg" id="svgFloor2" viewBox="0 0 1197.871 752.334">
               <g id="Group_1510" data-name="Group 1510" transform="translate(-388.728 -139.211)">
                 <g id="_00_ACKERSTR" data-name="00_ACKERSTR" transform="translate(389.728 527.779)">
                   <g id="_00_ACKERSTR_HATCH" data-name="00_ACKERSTR_HATCH" transform="translate(-389.728 -2706.598)">
