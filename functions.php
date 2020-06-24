10794953ea27df6ff2c562237a0d0fff7abc24c8
Marco <marco@defrostudio.eu>
2020-06-24 15:44:39 +0200

    search

--- functions.php
+++ functions.php
@@ -186,18 +186,6 @@ function html5_search_form( $form ) {
 
 add_filter( 'get_search_form', 'html5_search_form' );
 
-function wpdocs_my_search_form( $form ) {
-    $form = '<form role="search" method="get" id="searchform" class="searchform" action="' . home_url( '/' ) . '" >
-    <div><label class="screen-reader-text" for="s">' . __( 'Search for:' ) . '</label>
-    <input type="text" value="' . get_search_query() . '" name="s" id="s" />
-    <input type="submit" id="searchsubmit" value="'. esc_attr__( 'Search' ) .'" />
-    </div>
-    </form>';
- 
-    return $form;
-}
-add_filter( 'get_search_form', 'wpdocs_my_search_form' );
-
 
 /**
  * Implement the Custom Header feature.
