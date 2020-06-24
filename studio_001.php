b853d3cb0ace982ced56465a8713994a04276661
Marco <marco@defrostudio.eu>
2020-06-24 18:31:35 +0200

    seminar code close

--- studio_001.php
+++ studio_001.php
@@ -84,8 +84,7 @@
     <!-- Modal Seminar -->
     <div id="modalSeminar" class="modal">
         <div class="modal-content">
-            <span><?php the_field('seminar_code'); ?></span>
-            <span id="closeSeminar">&times;</span>
+            <span id="closeSeminar"><?php the_field('seminar_code'); ?></span>
             <div class="gallery_studio">
             <?php if( have_rows('seminar-gallery_studio') ): ?>
 
