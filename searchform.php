4ce2078c96aa6cca3d96ce08a8259c538b95f283
Marco <marco@defrostudio.eu>
2020-06-24 16:22:39 +0200

    search icon

--- searchform.php
+++ searchform.php
@@ -1,3 +1,3 @@
 <form id="searchform" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-    <input type="text" class="search-field" name="s" placeholder="S" value="<?php echo get_search_query(); ?>">
+    <input type="text" class="search-field" name="s" placeholder="&#x1F50D" value="<?php echo get_search_query(); ?>">
 </form>
