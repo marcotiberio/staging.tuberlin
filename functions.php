fb4a0bba099f549a3fadea67c64357c1fecfed31
Marco <marco@defrostudio.eu>
2020-06-24 15:04:39 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -174,7 +174,15 @@ add_action( 'wp_enqueue_scripts', 'wpb_add_google_fonts' );
 /**
  * Edit search bar.
  */
-
+function html5_search_form( $form ) { 
+	$form = '<section class="search">
+				<form role="search" method="get" id="search-form" action="' . home_url( '/' ) . '" >
+   					<label class="screen-reader-text" for="s">' . __('',  'domain') . '</label>
+					<input type="search" value="' . get_search_query() . '" name="s" id="s" placeholder="&#x1F50D;"/>
+				</form>
+			</section>';
+	return $form;
+}
 
 add_filter( 'get_search_form', 'html5_search_form' );
 
