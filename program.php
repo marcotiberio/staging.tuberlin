28281f2ef33b1fe1b8476a39e8d793fde60db2eb
Marco <marco@defrostudio.eu>
2020-06-25 16:32:10 +0200

    test program template

--- program.php
+++ program.php
@@ -9,6 +9,14 @@ get_header();
 
 	<main id="primary" class="site-main">
 
+		<div id="logoStudio">
+			<?php 
+				$image = get_field('logo_studio');
+				if( !empty( $image ) ): ?>
+					<img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+			<?php endif; ?>
+		</div>
+
 		<?php
 		while ( have_posts() ) :
 			the_post();
@@ -18,6 +26,16 @@ get_header();
 
 		endwhile; // End of the loop.
 		?>
+		<div class="footer">
+            <div class="bottom-left">
+                <a onclick="goBack()">
+                    <img src="http://staging-tuberlinifa.kinsta.cloud/wp-content/uploads/2020/06/MARC-T-STAIR.png" alt="">
+                </a>
+            </div>
+            <div class="bottom-right">
+                <img id="openInfo" src="http://staging-tuberlinifa.kinsta.cloud/wp-content/uploads/2020/06/info-icon.png" alt="Icon Info">
+            </div>
+        </div>
 
 	</main><!-- #main -->
 
