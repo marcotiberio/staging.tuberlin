de022e58da8c8b8cbf579e9d134495ef59280749
Marco <marco@defrostudio.eu>
2020-06-25 15:58:37 +0200

    test studio 002

--- studio_002.php
+++ studio_002.php
@@ -6,22 +6,31 @@
   
  get_header();  ?>
 
-<div class="wrap">
     
-    <div id="primary" class="content-area">
-		<main id="main" class="site-main">
+        <main id="primary" class="site-main">
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+        <?php
+        while ( have_posts() ) :
+            the_post();
 
-			get_template_part( 'template-parts/content-custom', get_post_type() );
+            get_template_part( 'template-parts/content', get_post_type() );
 
-		endwhile; // End of the loop.
-		?>
+            the_post_navigation(
+                array(
+                    'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'tu-berlin' ) . '</span> <span class="nav-title">%title</span>',
+                    'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'tu-berlin' ) . '</span> <span class="nav-title">%title</span>',
+                )
+            );
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+            // If comments are open or we have at least one comment, load up the comment template.
+            if ( comments_open() || get_comments_number() ) :
+                comments_template();
+            endif;
+
+        endwhile; // End of the loop.
+        ?>
+
+        </main><!-- #main -->
     
     <!-- CUSTOM POST LAYOUT -->
     <!-- CUSTOM POST LAYOUT -->
