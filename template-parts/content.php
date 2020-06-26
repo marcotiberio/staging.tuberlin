5abfd58d715d9daa22689c0d26ece9fc45b45673
Marco <marco@defrostudio.eu>
2020-06-26 15:02:31 +0200

    get title

--- template-parts/content.php
+++ template-parts/content.php
@@ -11,7 +11,7 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php echo get_the_title( $ID ); ?>
+		
 	</header><!-- .entry-header -->
 
 	<?php tu_berlin_post_thumbnail(); ?>
