b55ab04126515dd42e35d543ca8e8df519942f42
Marco <marco@defrostudio.eu>
2020-07-01 13:59:10 +0200

    floorpad mobile

--- template-parent.php
+++ template-parent.php
@@ -65,6 +65,45 @@ get_header();  ?>
 	</main><!-- #main -->
 
     <div class="site-main_mobile"> <!-- #main mobile -->
+            
+        <ul class="floor-pad" id="floorPad">
+          <li>
+            <a href="#">7</a>
+          </li>
+          <li>
+            <a href="#">8</a>
+          </li>
+          <li>
+            <a href="#">9</a>
+          </li>
+          <li>
+            <a href="#">4</a>
+          </li>
+          <li>
+            <a href="#">5</a>
+          </li>
+          <li>
+            <a href="#">6</a>
+          </li>
+          <li class="active">
+            <a href="#firstFloor">1</a>
+          </li>
+          <li>
+            <a href="#secondFloor">2</a>
+          </li>
+          <li>
+            <a href="#">3</a>
+          </li>
+          <li>
+            <a href="#"></a>
+          </li>
+          <li>
+            <a href="#">0</a>
+          </li>
+          <li>
+            <a href="#"></a>
+          </li>
+        </ul>
         
         <div class="logo-studio_mobile">
             <?php 
