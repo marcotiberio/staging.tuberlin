d18631183f83f9c01b01a3bf4b186cbadc9673e9
Marco <marco@defrostudio.eu>
2020-07-01 09:34:22 +0200

    page link

--- template-parent.php
+++ template-parent.php
@@ -7,11 +7,13 @@ get_header();  ?>
 	<main id="primary" class="site-main">
 
 		<div id="logoStudio">
-			<?php 
-				$image = get_field('logo_studio');
-				if( !empty( $image ) ): ?>
-					<img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
-			<?php endif; ?>
+            <a href="<?php the_field('page_link'); ?>">
+                <?php 
+                    $image = get_field('logo_studio');
+                    if( !empty( $image ) ): ?>
+                        <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+                <?php endif; ?>
+            </a>
 		</div>
 
 		<?php
