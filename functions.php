abe4c7dae5299853ff05481e4443fcde57ca1b7d
Marco <marco@defrostudio.eu>
2020-07-06 13:52:43 +0200

    registered slider block

--- functions.php
+++ functions.php
@@ -163,6 +163,34 @@ function tu_berlin_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'tu_berlin_scripts' );
 
+// Register a slider block.
+add_action('acf/init', 'my_register_blocks');
+function my_register_blocks() {
+
+    // check function exists.
+    if( function_exists('acf_register_block_type') ) {
+
+        // register a testimonial block.
+        acf_register_block_type(array(
+            'name'              => 'slider',
+            'title'             => __('Slider'),
+            'description'       => __('A custom slider block.'),
+            'render_template'   => 'template-parts/blocks/slider/slider.php',
+			'category'          => 'formatting',
+			'icon' 				=> 'images-alt2',
+			'align'				=> 'full',
+			'enqueue_assets' 	=> function(){
+				wp_enqueue_style( 'slick', 'http://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css', array(), '1.8.1' );
+				wp_enqueue_style( 'slick-theme', 'http://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css', array(), '1.8.1' );
+				wp_enqueue_script( 'slick', 'http://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js', array('jquery'), '1.8.1', true );
+
+				wp_enqueue_style( 'block-slider', get_template_directory_uri() . '/template-parts/blocks/slider/slider.css', array(), '1.0.0' );
+				wp_enqueue_script( 'block-slider', get_template_directory_uri() . '/template-parts/blocks/slider/slider.js', array(), '1.0.0', true );
+			  },
+        ));
+    }
+}
+
 /**
  * Enqueue fonts.
  */
