535ed510295189ef84564f8a06db32d0bc77e95d
Marco <marco@defrostudio.eu>
2020-07-07 15:39:28 +0200

    types template

---
+++ template_types.php
@@ -0,0 +1,37 @@
+<?php 
+	/* Template Name: Types */
+?>
+
+<?php get_header(); ?>
+
+
+<section class="background_landing">
+
+    <div class="intro-types">
+        <div id="titleTypes">
+            <h1>TYPES</h1>
+        </div>
+        <div id="infoTypes">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
+            et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
+            aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
+            dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
+            officia deserunt mollit anim id est laborum.</p>
+        </div>
+    </div>
+
+    <?php
+		while ( have_posts() ) :
+			the_post();
+
+			get_template_part( 'template-parts/content', 'page' );
+
+		endwhile; // End of the loop.
+	?>
+
+</section>
+
+
+<?php
+get_sidebar();
+get_footer();
