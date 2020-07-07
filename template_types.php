87229159b5d63b058cbf90ac0208c3aa5015192e
Marco <marco@defrostudio.eu>
2020-07-07 16:46:22 +0200

    of knowledge div

--- template_types.php
+++ template_types.php
@@ -25,6 +25,9 @@
         dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
         officia deserunt mollit anim id est laborum.</p>
     </div>
+    <div id="knowledgeTypes">
+        <h1>of knowledge</h1>
+    </div>
 </div>
 
 
