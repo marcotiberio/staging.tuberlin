ce4a12cc3fe826aaccc41d908c0e5f230f82ede8
Marco <marco@defrostudio.eu>
2020-07-07 17:17:40 +0200

    knowledge margin

--- js/script.js
+++ js/script.js
@@ -191,7 +191,7 @@ $(document).ready(function(){
     $(document).ready(function(){
     $( "#arrowsTypes" ).click(function() {
         $( "#knowledgeTypes" ).animate({
-            margin: "-18% 0 0 0", 
+            margin: "-22% 0 0 0", 
             }, 200 );
         });
     });
