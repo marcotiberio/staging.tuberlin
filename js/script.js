dec358faac427db0f03cdbb2d77bbac8aa0302c2
Marco <marco@defrostudio.eu>
2020-07-07 19:38:05 +0200

    80 px margin arrows

--- js/script.js
+++ js/script.js
@@ -159,7 +159,7 @@ $(document).ready(function(){
     $(document).ready(function(){
     $( "#arrowsTypes" ).click(function() {
         $( "#arrowOne" ).animate({
-            margin: "0 60px", 
+            margin: "0 80px", 
             }, 200 );
         });
     });
@@ -167,7 +167,7 @@ $(document).ready(function(){
     $(document).ready(function(){
     $( "#arrowsTypes" ).click(function() {
         $( "#arrowTwo" ).animate({
-            margin: "0 60px", 
+            margin: "0 80px", 
             }, 200 );
         });
     });
@@ -175,7 +175,7 @@ $(document).ready(function(){
     $(document).ready(function(){
     $( "#arrowsTypes" ).click(function() {
         $( "#arrowThree" ).animate({
-            margin: "0 60px", 
+            margin: "0 80px", 
             }, 200 );
         });
     });
