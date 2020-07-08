43a5a7bbcb06001e0714ba07d8ca3ec7555d5d04
Marco <marco@defrostudio.eu>
2020-07-08 09:35:37 +0200

    types mobile

--- template_types.php
+++ template_types.php
@@ -34,24 +34,51 @@
     </div>
 </div>
 
+<div class="intro-types" id="boxTypesMobile">
+    <div class="info-type">
+        <div id="titleTypes">
+            <h1>Types</h1><br>
+            <h1>of Knowledge Production</h1>
+        </div>
+        <div id="arrowsTypes">
+            <h1>
+            <span id="arrowOne"> > </span>
+            <span id="arrowTwo"> > </span>
+            <span id="arrowThree"> > </span>
+            </h1>
+        </div>
+        <div></div>
+    </div>
+    <div id="infoTypes">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
+        et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
+        aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
+        dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
+        officia deserunt mollit anim id est laborum.</p>
+    </div>
+    <div id="knowledgeTypes">
+        <h1>of Knowledge Production</h1>
+    </div>
+</div>
+
 <div style="display: none;">
 
-<?php if( have_rows('preview_movie') ): ?>
+    <?php if( have_rows('preview_movie') ): ?>
 
-    <?php while( have_rows('preview_movie') ): the_row(); 
+        <?php while( have_rows('preview_movie') ): the_row(); 
 
-        // vars
-        $cover = get_sub_field('cover_image');
-        $title = get_sub_field('title');
-        ?>
+            // vars
+            $cover = get_sub_field('cover_image');
+            $title = get_sub_field('title');
+            ?>
 
-        
-        <img src="<?php echo $cover['url']; ?>" />
-        <a href="<?php echo $title['url']; ?>" target="_blank" rel="noopener noreferrer"></a>
+            
+            <img src="<?php echo $cover['url']; ?>" />
+            <a href="<?php echo $title['url']; ?>" target="_blank" rel="noopener noreferrer"></a>
 
-    <?php endwhile; ?>
+        <?php endwhile; ?>
 
-<?php endif; ?>
+    <?php endif; ?>
 
 </div>
 
