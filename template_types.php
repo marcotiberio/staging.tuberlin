6d475685b10930a37897643563751da608338364
Marco <marco@defrostudio.eu>
2020-07-08 09:53:10 +0200

    mobile ids

--- template_types.php
+++ template_types.php
@@ -45,10 +45,10 @@
             </div>
         </div>
         <div id="rowTwo">
-            <div id="knowledgeTypes">
+            <div id="knowledgeTypesMobile">
                 <h1>of Knowledge Production</h1>
             </div>
-            <div id="infoTypes">
+            <div id="infoTypesMobile">
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
                 et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
                 aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
