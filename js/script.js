d55b3582dcc199a13e006dfe548e1566575c7fb5
Marco <marco@defrostudio.eu>
2020-07-08 10:05:07 +0200

    70vh mobile types

--- js/script.js
+++ js/script.js
@@ -249,7 +249,7 @@ $(document).ready(function(){
 $(document).ready(function(){
     $( "#arrowsTypesMobile" ).click(function() {
         $( "#boxTypesMobile" ).animate({
-            height: "65vh", 
+            height: "70vh", 
             }, 200 );
         });
     });
