988e275d85ebacac1fa29c63d775334d5401ee39
Marco <marco@defrostudio.eu>
2020-07-09 14:34:54 +0200

    script

--- js/script.js
+++ js/script.js
@@ -6,18 +6,6 @@ $(document).ready(function(){
     });
 });
 
-// $(document).ready(function(){
-//   $("#open_menuOverlay").click(function(){
-//     $("#menuOverlay").slideToggle("fast");
-//   });
-// });
-
-// $(document).ready(function(){
-//   $("#menu-toggleMobile").click(function(){
-//     $("#menuOverlay").slideToggle("fast");
-//   });
-// });
-
 
 // Studio Programs Modals
 $(document).ready(function(){
@@ -261,3 +249,15 @@ $(document).ready(function(){
             }, 200 );
         });
     });
+
+
+// FOYER
+
+$(document).ready(function(){
+    $("#_00_URBAN_DESIGN_FLOOR").mouseenter(function(){
+      $("#_00_URBAN_DESIGN_TEXT").css("display", "block");
+    });
+    $("#_00_URBAN_DESIGN_FLOOR").mouseleave(function(){
+      $("#_00_URBAN_DESIGN_TEXT").css("display", "none");
+    });
+});
