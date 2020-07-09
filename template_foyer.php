ce906f93d49835ed03463fc7b636a8c9fe9dd35b
Marco <marco@defrostudio.eu>
2020-07-09 14:18:02 +0200

    test

--- template_foyer.php
+++ template_foyer.php
@@ -2450,6 +2450,15 @@
       $("#_00_URBAN_DESIGN_TEXT").hide();
     });
 
+    $(document).ready(function(){
+      $("#_00_URBAN_DESIGN_FLOOR").mouseenter(function(){
+        $("#_00_URBAN_DESIGN_TEXT").css("display", "block");
+      });
+      $("#_00_URBAN_DESIGN_FLOOR").mouseleave(function(){
+        $("#_00_URBAN_DESIGN_TEXT").css("display", "none");
+      });
+    });
+
 
     function activeFloor(elem) {
     var a = document.getElementsByTagName('a')
