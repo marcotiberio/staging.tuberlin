3787450caffd1019f4b4b83df963b8762c946582
Marco <marco@defrostudio.eu>
2020-07-09 14:32:56 +0200

    added and enqued foyer.js

---
+++ js/foyer.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#_00_URBAN_DESIGN_FLOOR").mouseenter(function(){
+      $("#_00_URBAN_DESIGN_TEXT").css("display", "block");
+    });
+    $("#_00_URBAN_DESIGN_FLOOR").mouseleave(function(){
+      $("#_00_URBAN_DESIGN_TEXT").css("display", "none");
+    });
+});
