e50c52392a2b42a7af02033c550d2595e45d66c8
Marco <marco@defrostudio.eu>
2020-07-10 17:39:11 +0200

    link pdf no img

--- template-child.php
+++ template-child.php
@@ -76,7 +76,6 @@ get_header();  ?>
                     $file = get_field('pdf');
                     if( $file ): ?>
                         <a href="<?php echo $file['url']; ?>" target="_blank">
-                            <span><img src="http://staging-tuberlinifa.kinsta.cloud/wp-content/uploads/2020/07/download-button@2x.png" alt="Download Button"></span>
                             <h3>PDF</h3>
                         </a>
                     <?php endif; ?>
