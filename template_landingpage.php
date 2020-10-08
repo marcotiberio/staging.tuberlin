475e7aaec82c6bf07dcc5e759dfd30c15e74fc39
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-10-08 11:47:49 +0200

    fixed url issue

--- template_landingpage.php
+++ template_landingpage.php
@@ -16,7 +16,7 @@
 	?>
 
     <div class="button_logo-landing">
-		<a href="http://staging-tuberlinifa.kinsta.cloud/rundgang/">
+		<a href="http://tuberlinifa.kinsta.cloud/">
             <img src="http://staging-tuberlinifa.kinsta.cloud/wp-content/uploads/2020/06/logo.png" alt="TU Berlin IfA Logo">
         </a>
     </div>
